0cde6e2486cc1663215b480b886be4a8ad914660
over-geek <kenagy100@gmail.com>
2024-02-24 01:49:52 +0000

    generate boat controller

---
+++ app/controllers/api/boats_controller.rb
@@ -0,0 +1,31 @@
+class Api::BoatsController < ApplicationController
+	before_action :set_boat, only: [:show]
+
+    def index
+      @boats = Boat.all
+      render json: @boats, status: :ok
+    end
+
+    def show
+      render json: @boat
+    end
+
+    def create
+      @boat = Boat.new(boat_params)
+      if @boat.save
+        render json: @boat, status: :created
+      else
+        render json: @boat.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_boat
+      @boat = Boat.find(params[:id])
+    end
+
+    def boat_params
+      params.require(:boat).permit(:name, :description, :image, :availability, :duration, :finance_fee, :option_to_purchase, :amount_payable)
+    end
+end
