25c807e77a11fd08d961a58e9ddbe9049e0aad48
unknown <macherki.nessrine@yahoo.com>
2024-03-02 14:22:56 +0100

    fix linters error

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,49 +1,48 @@
 class Api::UsersController < ApplicationController
-    def index
-      @users = User.all
-      render json: @users
-    end
-  
-    def show
-      @user = User.find_by(name: params[:id])
-  
-      if @user
-        render json: { user: @user }
-      else
-        render json: { error: 'User not found' }, status: :not_found
-      end
-    end
-  
-    def create
-      @user = User.new(user_params)
-  
-      if @user.save
-        render json: { user: @user }, status: :created
-      else
-        render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
+  def index
+    @users = User.all
+    render json: @users
+  end
 
-    def login
-      name = params[:name] # Assuming the name is passed in the request parameters
-  
-      @user = User.find_by(name: name)
-  
-      if @user
-        render json: { data: @user }
-      else
-        render json: { error: 'Unauthorized' }, status: :unauthorized
-      end
-    end
+  def show
+    @user = User.find_by(name: params[:id])
 
-    def logout
-      render json: { message: 'Logout successful' }
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:name)
+    if @user
+      render json: { user: @user }
+    else
+      render json: { error: 'User not found' }, status: :not_found
     end
   end
-  
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: { user: @user }, status: :created
+    else
+      render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    name = params[:name] # Assuming the name is passed in the request parameters
+
+    @user = User.find_by(name:)
+
+    if @user
+      render json: { data: @user }
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
+
+  def logout
+    render json: { message: 'Logout successful' }
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
